6bced7f638395c3d2140b5a9ac59fd08235d8e2d
amiltonbandeira <amiltonbraulioadaobandeira@gmail.com>
2025-03-28 14:40:35 +0100

    [MOD] adapting the models to what is requested
    Took 13 minutes

--- models/mail_message.py
+++ models/mail_message.py
@@ -1,20 +1,24 @@
-from odoo import models, api, fields
+    from odoo import models, api, fields
 
+    class MailMessage(models.Model):
+        _inherit = 'mail.message'
 
+        memo_id = fields.Many2one("memo.log", string='Memo')
 
-class MailMessage(models.Model):
-    _inherit = 'mail.message'
-
-    memo_id = fields.Many2one("memo.log", 'Memo')
-
-    @api.model
-    def create_memo_message(self,message_id):
+        @api.model
+        def create_memo_message(self, message_id):
             message = self.browse(message_id)
+
             memo_vals = {
-                'name' : message.name or 'Memo from message ',
-                'recipient_type' : message.body or '',
+                'name': message.subject or 'Memo from message',
+                'recipient_type': message.body or '',
+                'related_document': message.model,
+                'author_id': message.author_id.id,
             }
 
             memo = self.env['memo.log'].create(memo_vals)
+
+            for attachment in message.attachment_ids:
+                memo.write({'message_attachment_ids': [(4, attachment.id)]})
 
             return memo
