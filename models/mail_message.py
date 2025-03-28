6cbec08687e895e36f9680b504b5b40e51e99f71
amiltonbandeira <amiltonbraulioadaobandeira@gmail.com>
2025-03-28 16:51:44 +0100

    [UPD] change the model mail.message to generate the memo after the normal message
    Took 8 minutes

--- models/mail_message.py
+++ models/mail_message.py
@@ -9,16 +9,23 @@ class MailMessage(models.Model):
     def create(self, vals):
 
         message = super(MailMessage, self).create(vals)
-        if not vals('memo_id'):
 
-            memo = self.env['memo.log'].create({
+        self.create_memo(message)
+        return message
+
+    def create_memo(self,message):
+
+        if not message.memo_id:
+
+            memo_vals = self.env['memo.log'].create({
                 'name':'Memo for message',
                 'recipient_type': 'document',
                 'related_document': 'Email',
                 'author_id': 'author',
             })
 
-            vals['memo_id'] = memo.id
+            memo = self.env['memo.log'].create(memo_vals)
 
+            message.memo_id = memo.id
 
-        return super(MailMessage, self).create(vals)
+        return message.memo_id
