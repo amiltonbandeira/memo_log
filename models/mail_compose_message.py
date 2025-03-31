dd95bd3e31848d6d9ca917228a5ab0b865f7bba1
amiltonbandeira <amiltonbraulioadaobandeira@gmail.com>
2025-03-31 16:15:27 +0100

    [MOD]
    Took 4 minutes

--- models/mail_compose_message.py
+++ models/mail_compose_message.py
@@ -1,8 +1,11 @@
-from odoo import models , fields
+from odoo import models, fields
 
 class MailComposeMessage(models.Model):
     _inherit = 'mail.compose.message'
 
-    fields.Many2one("res.partner", string="To" )
-    recipient_category = fields.Selection([('practitioner', 'Practitioner'), ('administrative', 'Administrative'), ('client', 'Client')], required=True, string='Recipient Category')
+    recipient_category = fields.Selection([
+        ('practitioner', 'Practitioner'),
+        ('administrative', 'Administrative'),
+        ('client', 'Client')
+    ], required=True, string='Recipient Category')
     create_memo = fields.Boolean(string='Create Memo')
