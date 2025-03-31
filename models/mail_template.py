351cf76d8d30e370fc207abc682cca56b5350c7d
amiltonbandeira <amiltonbraulioadaobandeira@gmail.com>
2025-03-31 14:05:56 +0100

    [UPD] working specifically in the mail template to ensure this changes in the mail.template model
    Took 16 minutes

--- models/mail_template.py
+++ models/mail_template.py
@@ -3,33 +3,5 @@ from odoo import models, fields
 class MailTemplate(models.Model):
     _inherit = 'mail.template'
 
-    recipient_type = fields.Char('Recipient Type',
-                                 help="Type of the recipient for the email (e.g., 'Customer', 'Internal')")
-    save_as_memo = fields.Boolean('Save as Memo', default=False,
-                                  help="If checked, the email will be saved as a memo after sending.")
-
-    def send_mail(self, template_id, model_id=None, res_id=None, force_send=False, email_values=None):
-
-        result = super(MailTemplate, self).send_mail(template_id, model_id, res_id, force_send, email_values)
-
-        if self.save_as_memo:
-            record = self.env[model_id].browse(res_id)
-
-            self.create_memo(record)
-
-        return result
-
-    def create_memo(self, record):
-
-        memo_vals = {
-            'name': 'Memo for message',
-            'recipient_type': 'document',
-            'related_document': 'Email',
-            'author_id': 'author',
-        }
-        memo = self.env['memo.log'].create(memo_vals)
-
-        if hasattr(record, 'memo_ids'):
-            record.memo_ids = [(4, memo.id)]
-
-        return memo
+    recipient_category = fields.Selection([('practitioner', 'Practitioner'), ('administrative', 'Administrative'), ('client', 'Client')], required=True, string='Recipient Category')
+    create_memo = fields.Boolean(string='Create Memo')
