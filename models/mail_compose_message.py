52fe62a2355f5b0ea8884050b935feccbc64b8b0
amiltonbandeira <amiltonbraulioadaobandeira@gmail.com>
2025-03-31 16:29:58 +0100

    [MOD]

--- models/mail_compose_message.py
+++ models/mail_compose_message.py
@@ -1,11 +1,11 @@
 from odoo import models, fields
 
-class MailComposeMessage(models.Model):
+class MailComposeMessage(models.TransientModel):
     _inherit = 'mail.compose.message'
 
     recipient_category = fields.Selection([
         ('practitioner', 'Practitioner'),
         ('administrative', 'Administrative'),
         ('client', 'Client')
-    ], required=True, string='Recipient Category')
+    ], required=True, string='Recipient Category', readonly=True)
     create_memo = fields.Boolean(string='Create Memo')
